30ebd97197ce6c541efbe7f7178cd5dc89632c80
Davi Machado Moureira <davi20021@outlook.com>
2025-07-16 09:35:44 -0300

    Opção de gerar resposta com o Gemini! (Possui um limite gratuito muito mais generoso, e geralmente, com respostas melhores!)

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import os
 import time
 from openai import OpenAI
 from requests.exceptions import ConnectionError
+from google import genai
+
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # Your keys must be in a .env file
 
 USER_ID = int(os.getenv("USER_ID"))
 
-openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY")) # If u want to use OpenAI API
+gemini_client = genai.Client(api_key=os.getenv("GEMINI_API_KEY")) # Or Gemini API (much better free tier)
 
 twitter_client = tweepy.Client(
     bearer_token=os.getenv("BEARER_TOKEN"),
@@ -19,7 +22,7 @@ twitter_client = tweepy.Client(
     access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
 )
 
-def gerar_resposta(tweet_text):
+def gerar_resposta_openai(tweet_text):
     if not tweet_text:
         return None
     
@@ -35,6 +38,23 @@ def gerar_resposta(tweet_text):
         print(f"Erro com a OpenAI API: {type(e).__name__} - {e}")
         return None
 
+def gerar_reposta_gemini(tweet_text):
+    if not tweet_text:
+        return None
+    
+    prompt = f"Responda de forma engraçada, amigável e breve ao seguinte tweet:\n{tweet_text}"
+    try:
+        resposta = gemini_client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents = prompt
+        )
+        return resposta.text
+    
+    except Exception as e:
+        print(f"Erro com a API Gemini: {type(e).__name__} - {e}")
+        return None
+
+
 ultimo_tweet_id = None
 
 print("Bot iniciado... monitorando a conta.")
@@ -49,7 +69,7 @@ while True:
             if tweet.id != ultimo_tweet_id and not tweet.text.startswith("RT "):
                 print(f"Novo tweet detectado: {tweet.text}")
                 
-                resposta = gerar_resposta(tweet.text)
+                resposta = gerar_reposta_gemini(tweet.text)
                 if resposta and resposta.strip():
                     twitter_client.create_tweet(
                         text=resposta,
@@ -65,7 +85,8 @@ while True:
         else:
             print("Nenhum tweet encontrado na conta.")
 
-        time.sleep(300)
+        print("Tweet respondido! Timeout de 6 minutos...")
+        time.sleep(6 * 60)
 
     except tweepy.TooManyRequests:
         print("Limite de requisições atingido. Timeout de 15 minutos...")
@@ -78,3 +99,4 @@ while True:
     except Exception as e:
         print(f"Não sei o que deu: {type(e).__name__} - {e}")
         time.sleep(60)
+
